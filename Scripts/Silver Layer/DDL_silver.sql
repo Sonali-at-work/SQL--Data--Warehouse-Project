343f3f3382c077831356f7b90967213fc756e12c
Sonali Patel <102735099+Sonali-at-work@users.noreply.github.com>
2026-02-07 19:38:03 +0530

    Add files via upload

---
+++ Scripts/Silver Layer/DDL_silver.sql
@@ -0,0 +1,133 @@
+IF OBJECT_ID('silver.olist_customers','U') IS NOT NULL
+   DROP TABLE silver.olist_customers
+GO
+CREATE TABLE silver.olist_customers (
+    customer_id              VARCHAR(50)  PRIMARY KEY ,
+    customer_unique_id       VARCHAR(50)  NOT NULL,
+    customer_zip_code_prefix INT,
+    customer_city            VARCHAR(100),
+    customer_state           CHAR(2),
+    dwh_create_date DATETIME2 default GETDATE()
+);
+
+IF OBJECT_ID('silver.olist_geolocation','U') IS NOT NULL
+   DROP TABLE silver.olist_geolocation
+GO
+CREATE TABLE silver.olist_geolocation (
+    geolocation_zip_code_prefix INT          NOT NULL,
+    geolocation_lat             DECIMAL(9,6) NOT NULL,
+    geolocation_lng             DECIMAL(9,6) NOT NULL,
+    geolocation_city            VARCHAR(100) NOT NULL,
+    geolocation_state           CHAR(2)      NOT NULL,
+    dwh_create_date DATETIME2 default GETDATE()
+
+);
+
+IF OBJECT_ID('silver.olist_order_items','U') IS NOT NULL
+   DROP TABLE silver.olist_order_items
+GO
+CREATE TABLE silver.olist_order_items(
+    order_id            VARCHAR(50)   NOT NULL,
+    order_item_id       INT           NOT NULL,
+    product_id          VARCHAR(50)   NOT NULL,
+    seller_id           VARCHAR(50)   NOT NULL,
+    shipping_limit_date DATETIME      NOT NULL,
+    price               DECIMAL(10,2) NOT NULL,
+    freight_value       DECIMAL(10,2) NOT NULL,
+    dwh_create_date DATETIME2 default GETDATE()
+
+    CONSTRAINT pk_olist_order_items 
+        PRIMARY KEY (order_id, order_item_id)
+);
+
+IF OBJECT_ID('silver.olist_order_payments','U') IS NOT NULL
+   DROP TABLE silver.olist_order_payments
+GO
+CREATE TABLE silver.olist_order_payments (
+    order_id              VARCHAR(50)   NOT NULL,
+    payment_sequential    INT           NOT NULL,
+    payment_type          VARCHAR(20)   NOT NULL,
+    payment_installments  INT           NOT NULL,
+    payment_value         DECIMAL(10,2) NOT NULL,
+    dwh_create_date DATETIME2 default GETDATE()
+
+    CONSTRAINT pk_olist_order_payments 
+        PRIMARY KEY (order_id, payment_sequential)
+);
+
+IF OBJECT_ID('silver.olist_order_reviews','U') IS NOT NULL
+   DROP TABLE silver.olist_order_reviews
+GO
+CREATE TABLE silver.olist_order_reviews (
+    review_id                VARCHAR(50)   ,
+    order_id                 VARCHAR(50)  NOT NULL,
+    review_score             INT,
+    review_comment_title     VARCHAR(255),
+    review_comment_message   VARCHAR(8000),
+    review_creation_date     DATETIME,
+    review_answer_timestamp  DATETIME,
+    dwh_create_date DATETIME2 default GETDATE()
+
+    
+);
+
+IF OBJECT_ID('silver.olist_orders','U') IS NOT NULL
+   DROP TABLE silver.olist_orders
+GO
+CREATE TABLE silver.olist_orders (
+    order_id                        CHAR(32)       primary key  ,
+    customer_id                     CHAR(32)        NOT NULL,
+    order_status                    VARCHAR(20)     NOT NULL,
+
+    order_purchase_timestamp        DATETIME        NOT NULL,
+    order_approved_at               DATETIME        ,
+    order_delivered_carrier_date    DATETIME        ,
+    order_delivered_customer_date   DATETIME        ,
+    order_estimated_delivery_date   DATETIME        NOT NULL,
+    dwh_create_date DATETIME2 default GETDATE()
+
+);
+
+IF OBJECT_ID('silver.olist_products','U') IS NOT NULL
+   DROP TABLE silver.olist_products
+GO
+   CREATE TABLE silver.olist_products (
+    product_id                      CHAR(32)       PRIMARY KEY  ,
+    product_category_name           VARCHAR(50)     NULL,
+
+    product_name_lenght             INT             NULL,
+    product_description_lenght      INT             NULL,
+    product_photos_qty              INT             NULL,
+
+    product_weight_g                INT             NULL,
+    product_length_cm               INT             NULL,
+    product_height_cm               INT             NULL,
+    product_width_cm                INT             NULL,
+    dwh_create_date DATETIME2 default GETDATE()
+
+
+);
+
+IF OBJECT_ID('silver.olist_sellers','U') IS NOT NULL
+   DROP TABLE silver.olist_sellers
+GO
+CREATE TABLE silver.olist_sellers (
+    seller_id               CHAR(52)       PRIMARY KEY  ,
+    seller_zip_code_prefix  INT             NOT NULL,
+    seller_city             VARCHAR(50)     NOT NULL,
+    seller_state            CHAR(2)          NOT NULL,
+    dwh_create_date DATETIME2 default GETDATE()
+
+
+);
+
+IF OBJECT_ID('silver.product_category_translation','U') IS NOT NULL
+   DROP TABLE silver.product_category_translation
+GO
+CREATE TABLE silver.product_category_translation (
+    product_category_name          VARCHAR(50)     PRIMARY KEY ,
+    product_category_name_english  VARCHAR(50)     NOT NULL,
+    dwh_create_date DATETIME2 default GETDATE()
+
+
+);
